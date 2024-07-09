adab29fb6446643104aa8ed24ac8328f7ba86e79
soup <lqh348659137@outlook.com>
2024-07-09 13:35:46 +0800

    feat: argo workflow

--- scripts/load-images.sh
+++ scripts/load-images.sh
@@ -7,8 +7,16 @@ docker tag mysql:8.0.32 $REGISTRY/$PROJECT/mysql:8.0.32
 docker tag bitnami/redis:6.2.12 $REGISTRY/$PROJECT/redis:6.2.12
 docker push $REGISTRY/$PROJECT/mysql:8.0.32
 docker push $REGISTRY/$PROJECT/redis:6.2.12
-## istio
+
+# istio
 docker tag docker.io/istio/pilot:1.22.2 $REGISTRY/istio/pilot:1.22.2
 docker tag docker.io/istio/proxyv2:1.22.2 $REGISTRY/istio/proxyv2:1.22.2
 docker push $REGISTRY/istio/pilot:1.22.2
-docker push $REGISTRY/istio/proxyv2:1.22.2
+
+# argo
+docker tag argoproj/argoexec:v3.4.3 $REGISTRY/argoproj/argoexec:v3.4.3
+docker tag argoproj/workflow-controller:v3.4.3 $REGISTRY/argoproj/workflow-controller:v3.4.3
+docker tag minio/minio:RELEASE.2023-04-20T17-56-55Z $REGISTRY/minio/minio:RELEASE.2023-04-20T17-56-55Z
+docker push $REGISTRY/argoproj/argoexec:v3.4.3
+docker push $REGISTRY/argoproj/workflow-controller:v3.4.3
+docker push $REGISTRY/minio/minio:RELEASE.2023-04-20T17-56-55Z
