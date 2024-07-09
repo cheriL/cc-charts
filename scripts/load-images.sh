d7c9c88a15a79d5ece1845af0171b4e62fed2ef9
soup <lqh348659137@outlook.com>
2024-07-09 11:39:25 +0800

    feat: add istio

--- scripts/load-images.sh
+++ scripts/load-images.sh
@@ -5,6 +5,10 @@ PROJECT=cube-studio
 
 docker tag mysql:8.0.32 $REGISTRY/$PROJECT/mysql:8.0.32
 docker tag bitnami/redis:6.2.12 $REGISTRY/$PROJECT/redis:6.2.12
-
 docker push $REGISTRY/$PROJECT/mysql:8.0.32
 docker push $REGISTRY/$PROJECT/redis:6.2.12
+## istio
+docker tag docker.io/istio/pilot:1.22.2 $REGISTRY/istio/pilot:1.22.2
+docker tag docker.io/istio/proxyv2:1.22.2 $REGISTRY/istio/proxyv2:1.22.2
+docker push $REGISTRY/istio/pilot:1.22.2
+docker push $REGISTRY/istio/proxyv2:1.22.2
